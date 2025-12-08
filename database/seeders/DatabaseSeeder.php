b58ace93ad111453cbb3edbf8c88f3ccf9f65cfc
nailah24ti@mahasiswa.pcr.ac.id <nailah24ti@mahasiswa.pcr.ac.id>
2025-12-08 21:49:00 +0700

    First Commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,10 +16,11 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Alec Thompson',
+            'email' => 'admin@corporateui.com',
+            'password' => Hash::make('secret'),
+            'about' => "Hi, I’m Alec Thompson, Decisions: If you can’t decide, the answer is no. If two equally difficult paths, choose the one more painful in the short term (pain avoidance is creating an illusion of equality).",
         ]);
     }
 }
